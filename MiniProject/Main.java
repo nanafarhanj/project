afc32d0c2bc9613e69f7003ee7426ba42320b17c
nanafarhanj <152027547+nanafarhanj@users.noreply.github.com>
2024-05-22 19:09:03 +0330

    Update Main.java
    run from terminal

--- MiniProject/Main.java
+++ MiniProject/Main.java
@@ -1,84 +1,150 @@
 package MiniProject;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Student student1=new Student("a","A",111,1);
-        Student student2=new Student("b","B",112,1);
-        Student student3=new Student("c","C",113,1);
-        Student student4=new Student("d","d",114,1);
-        Student student5=new Student("e","E",115,1);
-        Student student6=new Student("f","F",116,1);
-        Student student7=new Student("g","G",117,1);
-        Student student8=new Student("h","H",118,1);
-        Student student9=new Student("i","I",119,1);
-        Student student10=new Student("j","J",120,1);
-        Student student11=new Student("k","K",121,1);
-        Teacher teacher1 = new Teacher("a1","A1");
-        Teacher teacher2 = new Teacher("b1","B1");
-        Teacher teacher3 = new Teacher("c1","C1");
-        Teacher teacher4 = new Teacher("d1","D1");
-        Term term2=new Term(4022);
-        Course course1 = new Course("math1",teacher1,3,"2024-05-20");
-        Course course2 = new Course("AP",teacher2,5,"2024-05-21");
-        term2.addCourse(course1);
-        term2.addCourse(course2);
+    public static void main(String[] args) {
+        System.out.println("Hello...");
+        System.out.println("You are a teacher or a student? ");
+        System.out.println("if you are a teacher please enter 1 else enter 2");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        scanner.nextLine();
+        if (input == 1) {
+        while (true) {
+                Course course;
+                Assignment assignment;
+                Student student;
+                Teacher teacher = teacherMethod(scanner);
+                System.out.println("What do you wanna do?\nPlease enter the task's number");
+                System.out.println("1.addCourse\n2.removeCourse\n3.defineAssignment\n4.addStudent\n5.removeStudent\n6.deleteAssignment\n7.assignGrade");
+                System.out.println("if you want to exit the program please enter stop");
+                String input1 = scanner.nextLine();
+                if (input1.equals("stop")) {
+                    break;
+                }
+                switch (input1) {
+                    case "1":
+                        course = courseMethod(teacher, scanner);
+                        teacher.addCourse(course);
+                        break;
+                    case "2":
+                        course = courseMethod(teacher, scanner);
+                        teacher.removeCourse(course);
+                        break;
+                    case "3":
+                        course = courseMethod(teacher, scanner);
+                        assignment = assignmentMethod(scanner);
+                        teacher.defineAssignment(course, assignment);
+                        break;
+                    case "4":
+                        course = courseMethod(teacher, scanner);
+                        student = studentMethod(scanner);
+                        teacher.addStudent(course, student);
+                        break;
+                    case "5":
+                        course = courseMethod(teacher, scanner);
+                        student = studentMethod(scanner);
+                        teacher.removeStudent(course, student);
+                        break;
+                    case "6":
+                        course = courseMethod(teacher, scanner);
+                        assignment = assignmentMethod(scanner);
+                        teacher.deleteAssignment(course, assignment);
+                        break;
+                    case "7":
+                        course = courseMethod(teacher, scanner);
+                        assignment = assignmentMethod(scanner);
+                        student = studentMethod(scanner);
+                        System.out.println("Please enter student's grade");
+                        double grade = scanner.nextDouble();
+                        teacher.assignGrade(course, student, grade);
+                        break;
+                    default:
+                        System.out.println("Your request is not on the list");
+                        break;
+                }
+            }
+        }
+        else if (input == 2) {
+            while (true) {
+                Course course;
+                Assignment assignment;
+                Teacher teacher;
+                Student student = studentMethod(scanner);
+                System.out.println("What do you wanna do?\nPlease enter the task's number");
+                System.out.println("1.addCourse\n2.removeCourse\n3.getTotalUnits\n4.getOverallGPA\n5.getCurrentTermGPA\n6.getGradeForTheCourse\n7.printEnrolledCourses");
+                System.out.println("if you want to exit the program please enter stop");
+                String input1 = scanner.nextLine();
+                if (input1.equals("stop")) {
+                    break;
+                }
+                switch (input1) {
+                    case "1" :
+                        teacher = teacherMethod(scanner);
+                        course = courseMethod(teacher, scanner);
+                        student.addCourse(course);
+                        break;
+                    case "2" :
+                        teacher = teacherMethod(scanner);
+                        course = courseMethod(teacher, scanner);
+                        student.removeCourse(course);
+                        break;
+                    case "3" :
+                        System.out.println(student.getTotalUnits());
+                        break;
+                    case "4" :
+                        System.out.println(student.getOverallGPA());
+                         break;
+                    case "5" :
+                        System.out.println(student.getCurrentTermGPA());
+                        break;
+                    case "6" :
+                        teacher = teacherMethod(scanner);
+                        course = courseMethod(teacher, scanner);
+                        System.out.println(student.getGradeForTheCourse(course));
+                        break;
+                    case "7" :
+                        student.printEnrolledCourses();
+                        break;
+                    default:
+                        System.out.println("Your request is not on the list");
+                        break;
 
-          ArrayList<Course> courses=term2.getCourses();
-         for (Course course:courses) System.out.println(course.getName());
-
-
-       term2.removeCourse(course1);
-      ArrayList <Course> courses2=term2.getCourses();
-      for (Course course:courses) System.out.println(course.getName());
-
-     Teacher teacher=course1.getTeacher();
-      System.out.println(teacher.getFirstName()+" "+teacher.getLastName()+"\n"+"number of courses: "+teacher.getNumberOfCourses());
-      if (teacher.getCourses().get(0)==course1){
-       System.out.println("true");
-      }else {
-       System.out.println("false");
-      }
-      course1.setTeacher(teacher2);
-      System.out.println("number of courses for teacher1: "+teacher1.getNumberOfCourses());
-     System.out.println("number of courses for teacher2: "+teacher2.getNumberOfCourses());
-     teacher2.addStudent(course1,student2);
-     teacher2.addStudent(course2,student2);
-     teacher2.addStudent(course1,student3);
-     teacher2.removeStudent(course1,student3);
-     ArrayList<Student> students= course1.getStudents();
-     for (Student student: students){
-      System.out.println(student.getFirstName()+" "+student.getLastName());
-     }
-     teacher2.assignGrade(course1,student2,13.5);
-     System.out.println(student2.getGradeFoTheCourse(course1));
-     System.out.println(student2.getCourses().get(0).getName());
-     System.out.println(student2.getCurrentTermGPA());
-     teacher2.assignGrade(course2,student2,15.5);
-     System.out.println(student2.getCurrentTermGPA());
-     Assignment assignment1=new Assignment("first","2024-05-01",TypeAssignment.HOMEWORK);
-     Assignment assignment2=new Assignment("second","2024-05-06",TypeAssignment.PROJECT);
-     teacher2.defineAssignment(course2,assignment1);
-     teacher2.defineAssignment(course2,assignment2);
-     assignment1.howMuchTimeIsLeft();
-     teacher2.deleteAssignment(course2,assignment1);
-     ArrayList<Assignment>assignments=course2.getAssignments();
-     for(Assignment assignment: assignments){
-      System.out.println(assignment.getName()+" "+assignment.getDeadline()+" "+assignment.getTypeAssignment());
-     }
-     student2.printEnrolledCourses();
-     course2.printAssignmentDescription();
-     course2.printRegisteredStudents();
-     course2.addStudent(student3);
-     course2.addStudent(student4);
-     course2.addStudent(student5);
-     teacher2.assignGrade(course2,student3,19.5);
-     teacher2.assignGrade(course2,student4,18);
-     teacher2.assignGrade(course2,student5,19.5);//highest grade
-     course2.highestGrade();
-
-
+                }
+            }
+        }
     }
-
+    public static Teacher teacherMethod(Scanner scanner) {
+        System.out.println("Please enter your first name and last name");
+        String firstName = scanner.nextLine();
+        String lastName = scanner.nextLine();
+        return new Teacher(firstName, lastName);
+    }
+    public static Course courseMethod(Teacher teacher, Scanner scanner) {
+        System.out.println("please enter course's name, number of units and exam date in yyyy-MM-dd format");
+        String name = scanner.nextLine();
+        int units = scanner.nextInt();
+        scanner.nextLine();
+        String examDate = scanner.nextLine();
+        return new Course(name, teacher, units, examDate);
+    }
+    public static Assignment assignmentMethod(Scanner scanner) {
+        System.out.println("Please enter assignment's name, deadline in yyyy-MM-dd format and type");
+        String name = scanner.nextLine();
+        String deadLine = scanner.nextLine();
+        TypeAssignment typeAssignment = TypeAssignment.valueOf(scanner.nextLine());
+        return new Assignment(name, deadLine, typeAssignment);
+    }
+    public static Student studentMethod(Scanner scanner) {
+        System.out.println("Please enter student's first name, last name, id and current semester");
+        String firstName = scanner.nextLine();
+        String lastName = scanner.nextLine();
+        int id = scanner.nextInt();
+        int currentSemester = scanner.nextInt();
+        return new Student(firstName, lastName, id, currentSemester);
+    }
 }
+
+
